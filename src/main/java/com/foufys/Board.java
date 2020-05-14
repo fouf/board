62b8ee672827cbcc4a0917863198d93f7dcf6dca
Fouf <fouf@users.noreply.github.com>
2020-05-14 14:41:52 -0300

    board: update online status from text to symbol add click event to send online player a bird update example images update bin

--- src/main/java/com/foufys/Board.java
+++ src/main/java/com/foufys/Board.java
@@ -1,5 +1,9 @@
 package com.foufys;
 
+import net.md_5.bungee.api.chat.ClickEvent;
+import net.md_5.bungee.api.chat.ComponentBuilder;
+import net.md_5.bungee.api.chat.HoverEvent;
+import net.md_5.bungee.api.chat.TextComponent;
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
@@ -11,11 +15,11 @@ import java.util.logging.Level;
 
 public final class Board extends JavaPlugin {
 
-    public ChatColor LORD_COLOR = ChatColor.DARK_RED;
-    public ChatColor LEADERS_COLOR = ChatColor.AQUA;
-    public ChatColor BROTHERS_COLOR = ChatColor.BLUE;
-    public ChatColor ACOLYTES_COLOR = ChatColor.GREEN;
-    public ChatColor SERFS_COLOR = ChatColor.GRAY;
+    public net.md_5.bungee.api.ChatColor LORD_COLOR = net.md_5.bungee.api.ChatColor.DARK_RED;
+    public net.md_5.bungee.api.ChatColor LEADERS_COLOR = net.md_5.bungee.api.ChatColor.AQUA;
+    public net.md_5.bungee.api.ChatColor BROTHERS_COLOR = net.md_5.bungee.api.ChatColor.BLUE;
+    public net.md_5.bungee.api.ChatColor ACOLYTES_COLOR = net.md_5.bungee.api.ChatColor.GREEN;
+    public net.md_5.bungee.api.ChatColor SERFS_COLOR = net.md_5.bungee.api.ChatColor.GRAY;
 
     private BoardData boardData;
 
@@ -40,7 +44,7 @@ public final class Board extends JavaPlugin {
                 Player player = (Player) sender;
                 player.sendMessage(ChatColor.YELLOW + "--------BOARD v" + getDescription().getVersion() + "--------");
                 String currentTeam = "";
-                ChatColor teamColor = ChatColor.WHITE;
+                net.md_5.bungee.api.ChatColor teamColor = net.md_5.bungee.api.ChatColor.WHITE;
                 for (BoardData.AlivePlayer alivePlayer : boardData.GetAlivePlayers()) {
                     if (alivePlayer == null || alivePlayer.PlayerName == null) {
                         continue;
@@ -60,8 +64,17 @@ public final class Board extends JavaPlugin {
                     if (alivePlayer.RPName != null && !alivePlayer.RPName.equals("") && !alivePlayer.RPName.equals("NONE")) {
                         name = alivePlayer.RPName + " [" + alivePlayer.PlayerName + "]";
                     }
+                    TextComponent onlineIndicator = new TextComponent("█");
+                    onlineIndicator.setColor(onlineStatus == null ? net.md_5.bungee.api.ChatColor.RED : net.md_5.bungee.api.ChatColor.DARK_GREEN);
+                    if (onlineStatus != null) {
+                        onlineIndicator.setClickEvent(new ClickEvent(ClickEvent.Action.SUGGEST_COMMAND, "/bird " + alivePlayer.PlayerName));
+                        onlineIndicator.setHoverEvent(new HoverEvent( HoverEvent.Action.SHOW_TEXT, new ComponentBuilder( "Click to send a bird" ).create()));
+                    }
+                    TextComponent playerName = new TextComponent(name);
+                    playerName.setColor(teamColor);
+                    onlineIndicator.addExtra(playerName);
 
-                    player.sendMessage(teamColor + name + " - " + (onlineStatus == null ? ChatColor.RED + "OFFLINE" : ChatColor.DARK_GREEN + "ONLINE"));
+                    player.spigot().sendMessage(onlineIndicator);
                 }
                 player.sendMessage(ChatColor.YELLOW + "--------" + boardData.GetAlivePlayers().size() + " tracked alive players --------");
             }
@@ -70,7 +83,7 @@ public final class Board extends JavaPlugin {
         return false;
     }
 
-    public ChatColor getTeamColor(String teamName) {
+    public net.md_5.bungee.api.ChatColor getTeamColor(String teamName) {
         switch (teamName) {
             case "Lords":
                 return LORD_COLOR;
@@ -83,7 +96,7 @@ public final class Board extends JavaPlugin {
             case "Serfs":
                 return SERFS_COLOR;
             default:
-                return ChatColor.GRAY;
+                return net.md_5.bungee.api.ChatColor.GRAY;
         }
     }
 }
